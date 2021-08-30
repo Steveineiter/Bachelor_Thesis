b28b8fbebb4f585e12c6bbbbad34deca47b32a9c
Stefan Schoerkmeier <S.Schoerkmeier@student.tugraz.at>
2021-08-30 12:21:22 +0200

    Included URL in checklist, created personas with image.

--- Learning from Data/checklist/checklistCreator.py
+++ Learning from Data/checklist/checklistCreator.py
@@ -5,53 +5,53 @@ class checklistCreator:
     def create_checklist(self):
         # TODO Ask: what is in this case smarter? static methods like this or just as pycharm
         #  would handle it (extract it from class)?
-        id_of_post_to_post_was_liked_by = (
-            self.id_of_post_to_post_was_liked_by()
+        url_of_post_to_post_was_liked_by = (
+            self.url_of_post_to_post_was_liked_by()
         )
-        user_to_liked_posts_count_and_id = self.user_to_liked_posts_count_and_id(
-            id_of_post_to_post_was_liked_by
+        user_to_liked_posts_count_and_url = self.user_to_liked_posts_count_and_url(
+            url_of_post_to_post_was_liked_by
         )
-        self.safe_data_in_csv(user_to_liked_posts_count_and_id)
+        self.safe_data_in_csv(user_to_liked_posts_count_and_url)
 
     @staticmethod
-    def id_of_post_to_post_was_liked_by():
+    def url_of_post_to_post_was_liked_by():
         # open CSV
-        id_of_post_to_post_was_liked_by = {}
+        url_of_post_to_post_was_liked_by = {}
 
         with open("marry_items.csv", newline="") as marry_csv_file:
             dict_reader = csv.DictReader(marry_csv_file)
             # Put CSV data in dict (ID: liked by)
             for row in dict_reader:
-                print(row["id_of_post"], row["post_was_liked_by"])
-                id_of_post_to_post_was_liked_by[row["id_of_post"]] = row[
+                print(row["url_of_post"], row["post_was_liked_by"])
+                url_of_post_to_post_was_liked_by[row["url_of_post"]] = row[
                     "post_was_liked_by"
                 ]
 
-        return id_of_post_to_post_was_liked_by
+        return url_of_post_to_post_was_liked_by
 
     @staticmethod
-    def user_to_liked_posts_count_and_id(id_of_post_to_post_was_liked_by):
+    def user_to_liked_posts_count_and_url(url_of_post_to_post_was_liked_by):
         # Put names in dict and count it (Person: (counter, id_of_post))
-        user_to_liked_posts_count_and_id = {}
+        user_to_liked_posts_count_and_url = {}
 
-        for id_of_post, post_was_liked_by in id_of_post_to_post_was_liked_by.items():
+        for url_of_post, post_was_liked_by in url_of_post_to_post_was_liked_by.items():
             users = [str(user) for user in post_was_liked_by.split(" ")]
             for user in users:
-                if user in user_to_liked_posts_count_and_id:
-                    user_to_liked_posts_count_and_id[user] = (
-                        user_to_liked_posts_count_and_id[user][0] + 1,
-                        user_to_liked_posts_count_and_id[user][1] + " " + id_of_post,
+                if user in user_to_liked_posts_count_and_url:
+                    user_to_liked_posts_count_and_url[user] = (
+                        user_to_liked_posts_count_and_url[user][0] + 1,
+                        user_to_liked_posts_count_and_url[user][1] + " " + url_of_post,
                     )
                 else:
-                    user_to_liked_posts_count_and_id[user] = (1, id_of_post)
+                    user_to_liked_posts_count_and_url[user] = (1, url_of_post)
 
-        return user_to_liked_posts_count_and_id
+        return user_to_liked_posts_count_and_url
 
     @staticmethod
     def safe_data_in_csv(user_to_liked_posts_count_and_id):
         # Safe dict in CSV with columns Person, counter
         with open("marry_checklist.csv", "w", newline="") as marry_checklist_csv_file:
-            fieldnames = ["user", "liked_posts_count", "id_of_post"]
+            fieldnames = ["user", "liked_posts_count", "url_of_post"]
             dict_writer = csv.DictWriter(
                 marry_checklist_csv_file, fieldnames=fieldnames
             )
@@ -61,12 +61,12 @@ class checklistCreator:
                 user,
                 liked_posts_count_and_id,
             ) in user_to_liked_posts_count_and_id.items():
-                liked_posts_count, id_of_post = liked_posts_count_and_id
+                liked_posts_count, url_of_post = liked_posts_count_and_id
                 dict_writer.writerow(
                     {
                         "user": user,
                         "liked_posts_count": liked_posts_count,
-                        "id_of_post": id_of_post,
+                        "url_of_post": url_of_post,
                     }
                 )
 
