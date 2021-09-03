00282b5f8472a9690f97b2e57e21591f79cf6e2c
Stefan Schoerkmeier <S.Schoerkmeier@student.tugraz.at>
2021-09-03 10:16:13 +0200

    imporved clusterer speed

--- Learning from Data/z_Machine learning/Cluster analysis /leveshtein_cluster/cluster_analyser.py
+++ Learning from Data/z_Machine learning/Cluster analysis /leveshtein_cluster/cluster_analyser.py
@@ -1,11 +1,20 @@
 import csv
 
+import nltk
+
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+
 import pandas as pd
 
 from levenshtein_distance_calculator import LevenshteinDistanceCalculator
 from kruskals_mst import Graph
 
-MAX_HASHTAGS = 5
+# Constants to adjust
+MAX_HASHTAGS = 100
+WEIGHT_THRESHOLD = 2
+
+# Constants
 SOURCE = 0
 DESTINATION = 1
 WEIGHT = 2
@@ -34,19 +43,23 @@ class ClusterAnalyser:
         self.levenshtein_matrix = None
         self.index_to_word = dict()
         self.minimum_spanning_tree = list()
+        self.clusters = None
 
     # Read csv and extract hashtags
     def hashtags_from_csv(self):
         with open("used_hashtags.csv", newline="") as used_hashtags_csv_file:
             dict_reader = csv.DictReader(used_hashtags_csv_file)
             hashtag_counter = 0
+            all_stopwords = stopwords.words("english")
+
             # Put CSV data in dict (ID: liked by)
             for row in dict_reader:
                 hashtag = row["hashtag"].split("#")[1]
                 if hashtag != '' and hashtag != ' ' and len(self.list_of_hashtags) < MAX_HASHTAGS:
-                    self.list_of_hashtags.append(hashtag)
-                    self.index_to_word[str(hashtag_counter)] = hashtag
-                    hashtag_counter += 1
+                    if hashtag not in all_stopwords:
+                        self.list_of_hashtags.append(hashtag)
+                        self.index_to_word[str(hashtag_counter)] = hashtag
+                        hashtag_counter += 1
 
     # create levenshtein matrix
     def create_levenshtein_matrix(self):
@@ -67,35 +80,48 @@ class ClusterAnalyser:
                 if i != j:
                     graph.add_edge(i, j, self.levenshtein_matrix[i][j])
         graph.kruskal_minimum_spanning_tree()
+        print("Removing duplicates")
         self.minimum_spanning_tree = self.spanning_tree_without_duplicates(graph.graph)
 
     @staticmethod
     def spanning_tree_without_duplicates(graph):
-        cleaned_graph = list(list())
-        for edge in graph:
-            is_already_in_cleaned_graph = False
+        cleaned_graph = list()
 
-            if len(cleaned_graph) == 0:
-                cleaned_graph.append(edge)
+        # for edge in graph:
+        #     is_already_in_cleaned_graph = False
+        #
+        #     if len(cleaned_graph) == 0:
+        #         cleaned_graph.append(edge)
+        #         continue
+        #
+        #     for cleaned_edge in cleaned_graph:
+        #         if edge[SOURCE] == cleaned_edge[DESTINATION] and edge[DESTINATION] == cleaned_edge[SOURCE]:
+        #             is_already_in_cleaned_graph = True
+        #     if not is_already_in_cleaned_graph:
+        #         cleaned_graph.append(edge)
+        #
+        # return cleaned_graph
+
+        dictionary = {}
+        for (source, destination, weight) in graph:
+            if (destination, source, weight) in dictionary:
                 continue
+            else:
+                dictionary[(source, destination, weight)] = (source, destination, weight)
 
-            for cleaned_edge in cleaned_graph:
-                if edge[SOURCE] == cleaned_edge[DESTINATION] and edge[DESTINATION] == cleaned_edge[SOURCE]:
-                    is_already_in_cleaned_graph = True
-            if not is_already_in_cleaned_graph:
-                cleaned_graph.append(edge)
+        for edge in dictionary:
+            cleaned_graph.append(list(edge))
 
         return cleaned_graph
 
+
+
+
     def delete_elements_with_too_large_weight(self, threshold):
-        for edge in self.minimum_spanning_tree:
-            if edge[WEIGHT] >= threshold:
-                self.minimum_spanning_tree.remove(edge)
+        self.minimum_spanning_tree = [item for item in self.minimum_spanning_tree if item[WEIGHT] <= threshold]
 
     def create_cluster(self):
         clusters = list()
-        self.minimum_spanning_tree.append([10, 12, 1])# TODO Delte after testing
-        self.minimum_spanning_tree.append([12, 14, 2])# TODO Delte after testing
 
         for edge in self.minimum_spanning_tree:
             is_in_a_cluster = False
@@ -115,17 +141,48 @@ class ClusterAnalyser:
                 new_cluster.add_to_cluster(edge)
                 clusters.append(new_cluster)
 
-        print(clusters)
+        self.clusters = clusters
+
+    def safe_clusters_in_csv(self):
+        with open("clusters.csv", "w", newline="") as clusters_csv_file:
+            fieldnames = ["cluster", "used_hashtags"]
+            dict_writer = csv.DictWriter(
+                clusters_csv_file, fieldnames=fieldnames
+            )
+            dict_writer.writeheader()
+
+            counter = 1
+            for cluster in self.clusters:
+                elements = cluster.elements
+                hashtags = list()
+                for element in elements:
+                    hashtags.append(self.index_to_word[str(element)])
+
+                dict_writer.writerow(
+                    {
+                        "cluster": f"cluster {counter}",
+                        "used_hashtags": hashtags
+                    }
+                )
+                counter += 1
+
+
 
 
 if __name__ == "__main__":
     cluster_analyser = ClusterAnalyser()
     cluster_analyser.hashtags_from_csv()
-    cluster_analyser.create_levenshtein_matrix()
+    # cluster_analyser.create_levenshtein_matrix()
     cluster_analyser.levenshtein_matrix_from_csv("levenshtein_matrix.csv")
+    print("Creating minimum spanning tree")
     cluster_analyser.create_minimum_spanning_tree()
-    cluster_analyser.delete_elements_with_too_large_weight(9)
+    print("delete_elements_with_too_large_weight")
+    cluster_analyser.delete_elements_with_too_large_weight(WEIGHT_THRESHOLD)
+    print("create_cluster")
     cluster_analyser.create_cluster()
+    print("safe clusters in csv")
+    cluster_analyser.safe_clusters_in_csv()
+
 
 # TODO find out how to use this output
 # Output for 10: [
