c2bece6c4555535715f283e5accc1e357c3eafba
Stefan Schoerkmeier <S.Schoerkmeier@student.tugraz.at>
2021-09-11 09:38:13 +0200

    Created testing of the clusters and personas

--- learning_from_data/personas_with_hashtags/twitter/personaCategorizer.py
+++ learning_from_data/personas_with_hashtags/twitter/personaCategorizer.py
@@ -1,6 +1,7 @@
 import csv
 import glob
 import re
+from collections import defaultdict
 
 HASHTAGS_LINDA = (
     "#local #cooking #bouldering #sports #lesssugar #traveling #healthy "
@@ -20,56 +21,54 @@ HASHTAGS_CLAUDIA = (
     "#family #mum #local #familienleben #decoration #handmade #nature".split(" ")
 )
 
+CLUSER_INTERPRETATION_CSV = "/home/stefan/Knowledge/Bachelor-thesis/learning_from_data/personas_with_hashtags/twitter/clusters/clustering_interpretation_max_score_5_results.csv"
 
-# TODO Discuss: Should we also include the lifestyle stories? Also it may be better if they would be on german as well?
+
 class personaCategorizer:
     def __init__(self):
-        pass
+        self.top_20_clusters = self.top_20_clusters()
 
     def create_categorization(self):
         usernames_to_hashtags = self.usernames_to_hashtags()
+        usernames_to_clusters = self.usernames_to_clusters(usernames_to_hashtags)
         usernames_to_personas_and_hashtags = self.usernames_to_personas_and_hashtags(
             usernames_to_hashtags
         )
-        self.safe_data_in_csv(usernames_to_personas_and_hashtags)
+        usernames_to_data = self.merge_dicts(
+            usernames_to_personas_and_hashtags, usernames_to_clusters
+        )
+
+        self.safe_data_in_csv(usernames_to_data)
 
     @staticmethod
     def usernames_to_hashtags():
         usernames_to_hashtags = dict()
-        file_paths = glob.glob("data" + "/**/*.csv", recursive=True)
-        for path in file_paths:
-            parts_of_path = path.split("/")
-            username = parts_of_path[1]
-            type_of_file = parts_of_path[2]
+        files = glob.glob("data" + "/*.csv", recursive=True)
+        for file in files:
+            parts_of_path = file.split("_")
+            username = parts_of_path[0].split("/")[1]  # To get rid of data/
 
-            # TODO refactor, 2 pretty similar functions.
-            if type_of_file == "profile_data.csv":
-                with open(path, newline="") as profile_data_csv_file:
-                    dict_reader = csv.DictReader(profile_data_csv_file)
-                    for row in dict_reader:
-                        if username in usernames_to_hashtags.keys():
-                            usernames_to_hashtags[username] += row[
-                                "hashtags_of_description"
-                            ]
-                        else:
-                            usernames_to_hashtags[username] = row[
-                                "hashtags_of_description"
-                            ]
+            with open(file, newline="") as twitter_user_csv_file:
+                dict_reader = csv.DictReader(twitter_user_csv_file)
+                for row in dict_reader:
+                    if username in usernames_to_hashtags.keys():
+                        usernames_to_hashtags[username] += [
+                            row["Text"],
+                            row["Embedded_text"],
+                        ]
+                    else:
+                        usernames_to_hashtags[username] = [
+                            row["Text"],
+                            row["Embedded_text"],
+                        ]
 
-            if type_of_file == "posts_data.csv":
-                with open(path, newline="") as posts_data_csv_file:
-                    dict_reader = csv.DictReader(posts_data_csv_file)
-                    for row in dict_reader:
-                        if username in usernames_to_hashtags.keys():
-                            usernames_to_hashtags[username] += row["hashtags_of_post"]
-                        else:
-                            usernames_to_hashtags[username] = row["hashtags_of_post"]
-
-        for username, hashtags in usernames_to_hashtags.items():
-            hashtags = hashtags.split("#")[1:]
-            for index in range(len(hashtags)):
-                hashtags[index] = "#" + hashtags[index].strip()
-            usernames_to_hashtags[username] = hashtags
+        for username, text_of_posts in usernames_to_hashtags.items():
+            used_hashtags = list()
+            for text in text_of_posts:
+                hashtags = re.findall(r"#\w+", text)
+                for hashtag in hashtags:
+                    used_hashtags.append(hashtag)
+            usernames_to_hashtags[username] = used_hashtags
 
         return usernames_to_hashtags
 
@@ -77,12 +76,24 @@ class personaCategorizer:
         usernames_to_personas_and_hashtags = dict()
         for username, hashtags in usernames_to_hashtags.items():
             hashtags_of_user_without_duplicates = list(dict.fromkeys(hashtags))
-            like_linda = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_LINDA)
-            like_karl_peter = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_KARL_PETER)
-            like_sandy = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_SANDY)
-            like_kevin = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_KEVIN)
-            like_martin = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_MARTIN)
-            like_claudia = self.like_persona(hashtags_of_user_without_duplicates, HASHTAGS_CLAUDIA)
+            like_linda = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_LINDA
+            )
+            like_karl_peter = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_KARL_PETER
+            )
+            like_sandy = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_SANDY
+            )
+            like_kevin = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_KEVIN
+            )
+            like_martin = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_MARTIN
+            )
+            like_claudia = self.like_persona(
+                hashtags_of_user_without_duplicates, HASHTAGS_CLAUDIA
+            )
 
             usernames_to_personas_and_hashtags[username] = (
                 like_linda,
@@ -99,18 +110,78 @@ class personaCategorizer:
     @staticmethod
     def like_persona(hashtags_of_user, hashtags_of_persona):
         same_hashtags_count = 0
+        hashtags_of_persona_lower = list()
+        hashtags_of_user_lower = list()
 
+        for hashtag in hashtags_of_persona:
+            hashtags_of_persona_lower.append(hashtag.lower())
         for hashtag in hashtags_of_user:
-            if hashtag in hashtags_of_persona:
+            hashtags_of_user_lower.append(hashtag.lower())
+
+        for hashtag in hashtags_of_user_lower:
+            if hashtag in hashtags_of_persona_lower:
                 same_hashtags_count += 1
 
         return round((same_hashtags_count / len(hashtags_of_persona)) * 100, 2)
 
+    def usernames_to_clusters(self, usernames_to_hashtags):
+        usernames_to_clusters = dict()
+
+        for username, hashtags in usernames_to_hashtags.items():
+            best_cluster_top_20 = dict()
+            best_cluster_top_20_max_score = float("-inf")
+
+            hashtags_of_user_without_duplicates = list(dict.fromkeys(hashtags))
+            for top_20_cluster in self.top_20_clusters.items():
+                current_score = self.like_persona(
+                    hashtags_of_user_without_duplicates, top_20_cluster[1]
+                )
+                if current_score > best_cluster_top_20_max_score:
+                    best_cluster_top_20_max_score = current_score
+                    best_cluster_top_20 = (
+                        top_20_cluster[0],
+                        best_cluster_top_20_max_score,
+                    )
+
+            usernames_to_clusters[username] = best_cluster_top_20
+
+        return usernames_to_clusters
+
     @staticmethod
-    def safe_data_in_csv(usernames_to_personas_and_hashtags):
+    def top_20_clusters():
+        top_20_clusters = dict()
+        with open(CLUSER_INTERPRETATION_CSV, newline="") as twitter_user_csv_file:
+            dict_reader = csv.DictReader(twitter_user_csv_file)
+            for row in dict_reader:
+                cluster = row["best_clusters"]
+                if not cluster:
+                    break
+                used_hashtags = row["hashtags_of_best_clusters"].split(",")
+                hashtags = list()
+                for hashtag in used_hashtags:
+                    hashtag = re.sub(r"\W+", "", hashtag)
+                    if hashtag == "" or hashtag == " ":
+                        continue
+                    hashtags.append("#" + hashtag)
+                top_20_clusters[cluster] = hashtags
+
+        return top_20_clusters
+
+    @staticmethod
+    def merge_dicts(dict_one, dict_two):
+        default_dictionary = defaultdict(list)
+
+        for dictionary in (dict_one, dict_two):
+            for key, value in dictionary.items():
+                default_dictionary[key].append(value)
+
+        return default_dictionary
+
+    @staticmethod
+    def safe_data_in_csv(usernames_to_data):
         # Safe dict in CSV with columns Person, counter
         with open(
-                "persona_categorization.csv", "w", newline=""
+            "persona_categorization.csv", "w", newline=""
         ) as persona_categorization_csv_file:
             fieldnames = [
                 "username",
@@ -120,17 +191,29 @@ class personaCategorizer:
                 "like_kevin",
                 "like_martin",
                 "like_claudia",
+                "like_best_cluster_of_all",
+                "like_best_cluster_of_top_20",
                 "used_hashtags",
             ]
             dict_writer = csv.DictWriter(
                 persona_categorization_csv_file, fieldnames=fieldnames
             )
-
             dict_writer.writeheader()
+
+            score_of_cluster = 0
+            score_of_old_persona = 0
             for (
                 username,
-                liked_posts_count_and_id,
-            ) in usernames_to_personas_and_hashtags.items():
+                data,
+            ) in usernames_to_data.items():
+                tuple_one, tuple_two = data
+                if len(tuple_one) == 2:
+                    cluster_data = tuple_one
+                    persona_data = tuple_two
+                if len(tuple_one) == 7:
+                    persona_data = tuple_one
+                    cluster_data = tuple_two
+
                 (
                     like_linda,
                     like_karl_peter,
@@ -139,7 +222,8 @@ class personaCategorizer:
                     like_martin,
                     like_claudia,
                     used_hashtags,
-                ) = liked_posts_count_and_id
+                ) = persona_data
+
                 dict_writer.writerow(
                     {
                         "username": username,
@@ -149,9 +233,24 @@ class personaCategorizer:
                         "like_kevin": str(like_kevin) + "%",
                         "like_martin": str(like_martin) + "%",
                         "like_claudia": str(like_claudia) + "%",
+                        "like_best_cluster_of_all": None,  # TODO should we implement this? maybe worth for new clusters
+                        "like_best_cluster_of_top_20": str(
+                            f"{cluster_data[1]}%, Cluster: {cluster_data[0]}"
+                        ),
                         "used_hashtags": str(used_hashtags),
                     }
                 )
+                score_of_cluster += cluster_data[1]
+                score_of_old_persona += max(
+                    like_linda,
+                    like_karl_peter,
+                    like_sandy,
+                    like_kevin,
+                    like_martin,
+                    like_claudia,
+                    )
+            print(f"Aggregated % of cluster: {score_of_cluster:.2f}\n"
+                  f"Aggregated % of best persona: {score_of_old_persona:.2f}")
 
 
 if __name__ == "__main__":
